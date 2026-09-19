938c5141b69f0344086334510bb18d102e0978e3
kalyanigadade26 <kalyanigadade26@gmail.com>
2026-09-19 15:59:26 +0530

    add deployed backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy:{
       '/api':{
-        target: 'http://localhost:2607',
+        target: 'https://homelyhub-jkoa.onrender.com',
         changeOrigin: true,
         secure: false,
       }
