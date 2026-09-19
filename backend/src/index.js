4fe62edeae179a958b919c929234e782f961dbc3
kalyanigadade26 <kalyanigadade26@gmail.com>
2026-09-19 15:26:44 +0530

    Fix property router import path

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import {router} from "./routes/userRoutes.js";
-import { propertyRouter } from "../routes/propertyRouter.js";
+import { propertyRouter } from "./routes/propertyRouter.js";
 import { bookingRouter } from "./routes/bookingRouter.js";
 import { tripRouter } from "./routes/tripRouter.js";
 
